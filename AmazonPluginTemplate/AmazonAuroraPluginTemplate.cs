e92b54c86d2cd06968bcd0c2917e01d2a46268bf
Zaicon Kiroshu <kzaicon@yahoo.com>
2022-05-21 14:44:56 -0500

    Initial Commit

---
+++ AmazonPluginTemplate/AmazonAuroraPluginTemplate.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Reflection;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace PluginTemplate
+{
+    [ApiVersion(2, 1)]
+    public class AmazonAuroraPluginTemplate : TerrariaPlugin
+    {
+        public override string Author => "Zaicon";
+        public override string Description => "Best coded template plugin in... Aurora.";
+        public override string Name => "AmazonAuroraPluginTemplate";
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+        public AmazonAuroraPluginTemplate(Main game) : base(game) { }
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
+            }
+            base.Dispose(disposing);
+        }
+
+        private void OnInitialize(EventArgs args)
+        {
+            DatabaseManager.Connect();
+
+            Commands.ChatCommands.RemoveAll(e => e.Name == "help");
+            Commands.ChatCommands.Add(new Command("permission.here", CustomHelpCmdMethod, "help") { AllowServer = false });
+        }
+
+        private void CustomHelpCmdMethod(CommandArgs args)
+        {
+            args.Player.KillPlayer();
+
+            if (!args.Player.IsLoggedIn)
+            {
+                TShock.Utils.Broadcast($"{args.Player.Name} is very un-special and they can't count. Their IP is {args.Player.IP}.", new Color(155, 155, 155));
+            }
+            else
+            {
+                var cmdCount = DatabaseManager.IncreaseHelpCmdCount(args.Player.Account.ID);
+                TShock.Utils.Broadcast($"{args.Player.Name} is logged in as {args.Player.Account.Name} and their IP is {args.Player.IP}.", new Color(155, 155, 155));
+                TSPlayer.All.SendInfoMessage($"{args.Player.Name} has died {cmdCount} times now.");
+                TSPlayer.All.SendMessageFromPlayer("i hate u all, down with aurora, long live amazon!!11!11!!!!", args.Player.Group.R, args.Player.Group.G, args.Player.Group.B, args.Player.Index);
+            }
+        }
+    }
+}
---
+++ AmazonPluginTemplate/DatabaseManager.cs
@@ -0,0 +1,70 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using TShockAPI;
+using TShockAPI.DB;
+
+namespace PluginTemplate
+{
+    internal static class DatabaseManager
+    {
+        /// <summary>
+        /// Generate the connection string based on the information in TShock config settings.
+        /// </summary>
+        private static string ConnectionString
+        {
+            get
+            {
+                if (!TShock.Config.Settings.StorageType.Equals("mysql", StringComparison.OrdinalIgnoreCase))
+                    throw new NotImplementedException("This plugin only supports MYSQL databases.");
+
+                // localhost:3306
+                var hostTextRaw = TShock.Config.Settings.MySqlHost;
+                bool hasPortNumber = hostTextRaw.Contains(":");
+                string hostName = hostTextRaw.Split(':')[0];
+                string hostPort = hasPortNumber ? hostTextRaw.Split(':')[1] : "3306";
+                string databaseName = TShock.Config.Settings.MySqlDbName;
+                string databaseUsername = TShock.Config.Settings.MySqlUsername;
+                string databasePassword = TShock.Config.Settings.MySqlPassword;
+
+                return $"Server={hostName}; Port={hostPort}; Database={databaseName}; Uid={databaseUsername}; Pwd={databasePassword}";
+            }
+        }
+
+        public static void Connect()
+        {
+            MySqlConnection conn = new MySqlConnection(ConnectionString);
+
+            SqlTableCreator tableCreator = new SqlTableCreator(conn, new MysqlQueryCreator());
+
+            tableCreator.EnsureTableStructure(new SqlTable("amazon_usage",
+                new SqlColumn("ID", MySqlDbType.Int32, 6) { AutoIncrement = true, Primary = true, Unique = true },
+                    new SqlColumn("user_id", MySqlDbType.Int32, 8),
+                    new SqlColumn("cmd_count", MySqlDbType.Int32, 8)
+                ));
+        }
+
+        public static int IncreaseHelpCmdCount(int userId)
+        {
+            var conn = new MySqlConnection(ConnectionString);
+
+            // Get current count
+            int cmdCount = 0;
+
+            var result = conn.QueryFirstOrDefault("SELECT * FROM amazon_usage WHERE user_id = @userId;", new { userId });
+            if (result != null)
+                cmdCount = (int)result.cmd_count;
+            else
+                conn.Execute("INSERT INTO amazon_usage (user_id, cmd_count) VALUES (@userId, @cmdCount);",new { cmdCount = 1, userId });
+
+            // Increase count
+            cmdCount++;
+            conn.Execute("UPDATE amazon_usage SET cmd_count = @cmdCount WHERE user_id = @userId;", new { cmdCount, userId });
+
+            return cmdCount;
+        }
+    }
+}
